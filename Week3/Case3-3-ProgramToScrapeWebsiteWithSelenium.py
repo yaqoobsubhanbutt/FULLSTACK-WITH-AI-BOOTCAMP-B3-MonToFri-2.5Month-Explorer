ee9b3cd9cdf46039997c0db632999929d0eeef81
yaqoobsubhanbutt <yaqoobsubhanbutt@gmail.com>
2025-10-10 20:45:17 +0500

    Update Case3-3-ProgramToScrapeWebsiteWithSelenium.py

--- Week3/Case3-3-ProgramToScrapeWebsiteWithSelenium.py
+++ Week3/Case3-3-ProgramToScrapeWebsiteWithSelenium.py
@@ -5,7 +5,7 @@ import csv
 
 url = "http://www.values.com/inspirational-quotes"
 
-cService = webdriver.ChromeService(executable_path='C:\\Users\\HP\\Downloads\\chromedriver-win64 (1)\\chromedriver-win64\\chromedriver.exe') # '/Users/bpfalz/Downloads/chromedriver' for my macbook
+cService = webdriver.ChromeService(executable_path='C:\Users\LENOVO\Downloads\chromedriver-win64\chromedriver-win64\\chromedriver-win64\\chromedriver.exe') # '/Users/bpfalz/Downloads/chromedriver' for my macbook
 driver = webdriver.Chrome(service=cService)
 
 driver.get(url)
